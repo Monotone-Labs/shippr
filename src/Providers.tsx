427ddb7029d5f38b08b0564763a003243e450ad9
d-lite <olabemiwodaniel@gmail.com>
2024-01-19 14:02:23 +0100

    chore: add connect btn

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,10 +1,36 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import {ReactNode} from "react";
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {configureChains, createConfig, WagmiConfig} from 'wagmi';
+import { mainnet, polygon, sepolia } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const { chains, publicClient } = configureChains(
+    [mainnet, polygon, sepolia],
+    [publicProvider()],
+);
+
+const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains,
+});
+
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+});
 
 export const Providers = ({ children}: { children: ReactNode }) => {
       return (
         <ChakraProvider>
-          {children}
+            <WagmiConfig config={ wagmiConfig} >
+                <RainbowKitProvider chains={chains}>
+                    {children}
+                </RainbowKitProvider>
+            </WagmiConfig>
         </ChakraProvider>
       );
 }
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,6 +1,7 @@
-import {Box, Circle, Container, Flex, Image, Text, VStack} from "@chakra-ui/react";
+import {Box, Container, Flex, Image, Text, VStack} from "@chakra-ui/react";
 import LandingImage from "../assets/landing.png"
-import {ArrowForwardIcon} from "@chakra-ui/icons";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 const LandingPage = () => {
     return (
         <Container bgGradient='linear(to-r, #5B86E5, #36D1DC)' minW="100vw" h="100vh">
@@ -10,9 +11,8 @@ const LandingPage = () => {
                         <Text fontSize="72" fontWeight="bold">Shippr</Text>
                         <Text fontSize="24" fontWeight="semibold">Exquisite escrow service</Text>
 
-                            <Circle size='70px' mt="5" bg='white' color='blue'>
-                                <ArrowForwardIcon boxSize="10" />
-                            </Circle>
+                        <ConnectButton  />
+
                     </VStack>
                 </Box>
                 <Flex w="inherit" justify="center">
